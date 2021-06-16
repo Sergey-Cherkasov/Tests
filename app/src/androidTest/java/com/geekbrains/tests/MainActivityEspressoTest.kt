933b89fa8e6babe695c76e261a1593ed8420921d
Sergio Cherkasov <sergey.cherkasov.developer@gmail.com>
2021-06-16 17:49:50 +0700

    tests complete

--- app/src/androidTest/java/com/geekbrains/tests/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/MainActivityEspressoTest.kt
@@ -1,6 +1,8 @@
 package com.geekbrains.tests
 
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
@@ -10,6 +12,7 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.geekbrains.tests.view.search.MainActivity
+import junit.framework.TestCase
 import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
@@ -38,6 +41,74 @@ class MainActivityEspressoTest {
             onView(isRoot()).perform(delay())
             onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 2283")))
         }
+    }
+
+    @Test
+    fun activitySearch_edittext_not_null() {
+        scenario.onActivity {
+            val searchEditText = it.findViewById<TextView>(R.id.searchEditText)
+            TestCase.assertNotNull(searchEditText)
+        }
+    }
+
+    @Test
+    fun activitySearch_edittext_visible() {
+        onView(withId(R.id.searchEditText)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun activitySearch_edittext_displayed() {
+        onView(withId(R.id.searchEditText)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun activitySearch_edittext_completely_displayed() {
+        onView(withId(R.id.searchEditText)).check(matches(isCompletelyDisplayed()))
+    }
+
+    @Test
+    fun activitySearch_edittext_effective_visible() {
+        onView(withId(R.id.searchEditText))
+            .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+    }
+
+    @Test
+    fun activitySearch_edittext_text_inscription_correct() {
+        onView(withId(R.id.searchEditText)).check(matches(withHint("Enter keyword e.g. android")))
+    }
+
+    @Test
+    fun activitySearch_button_not_null() {
+        scenario.onActivity {
+            val toDetailsActivityButton = it.findViewById<Button>(R.id.toDetailsActivityButton)
+            TestCase.assertNotNull(toDetailsActivityButton)
+        }
+    }
+
+    @Test
+    fun activitySearch_button_visible() {
+        onView(withId(R.id.toDetailsActivityButton)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun activitySearch_button_displayed() {
+        onView(withId(R.id.toDetailsActivityButton)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun activitySearch_button_completely_displayed() {
+        onView(withId(R.id.toDetailsActivityButton)).check(matches(isCompletelyDisplayed()))
+    }
+
+    @Test
+    fun activitySearch_button_effective_visible() {
+        onView(withId(R.id.toDetailsActivityButton))
+            .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+    }
+
+    @Test
+    fun activitySearch_button_text_inscription_correct() {
+        onView(withId(R.id.toDetailsActivityButton)).check(matches(withText("to details")))
     }
 
     private fun delay(): ViewAction? {
